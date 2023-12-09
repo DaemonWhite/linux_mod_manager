326c2802fa7ab606ab80f7ff5f1dbd08e7ddf476
DaemonWhite <matheo.daemon@gmail.com>
2023-12-09 17:09:14 +0100

    Fix skyrim plugin

--- src/plugins/games/skyrim.py
+++ src/plugins/games/skyrim.py
@@ -4,5 +4,5 @@ class PluginGames(PluginGame):
     def __init__(self):
         super().__init__("Skyrim", "The Elder Scrolls V: Skyrim", 1, 0.1)
         self._nexus_mod = True
-        self._systeme = "win"
+        self._systeme = ["win"]
         self.symbolic = False
