233f8c3fcab4f2ff2c4ba795d1c205fae52dd9ac
DaemonWhite <matheo.daemon@gmail.com>
2024-05-11 18:54:26 +0200

    change: load use stape row

--- src/modal/load.py
+++ src/modal/load.py
@@ -2,6 +2,8 @@ from gi.repository import Adw
 from gi.repository import Gtk
 
 from py_mod_manager.const import UI_BASE
+from custom_widget.stape_row import StapeRow
+
 
 @Gtk.Template(resource_path=UI_BASE+'modal/load.ui')
 class PyModManagerWindowModalLoad(Adw.Window):
@@ -9,21 +11,33 @@ class PyModManagerWindowModalLoad(Adw.Window):
 
     load_stack = Gtk.Template.Child()
 
+    stape_configuration = Gtk.Template.Child()
+
     load_status = Gtk.Template.Child()
     result_status = Gtk.Template.Child()
-    search_spiner = Gtk.Template.Child()
 
     close_button = Gtk.Template.Child()
 
-    def __init__(self, windows):
-        Adw.Window.__init__(self, title="Choose Games")
+    def __init__(self, windows, name):
+        Adw.Window.__init__(self, title="Load")
         self.__window = windows
+        self.__stape = 0
+        self.__array_stape = []
         self.set_transient_for(self.__window)
-        self.search_spiner.start()
+
+        self.stape_configuration.set_title(name)
 
         self.accept_close = False
 
         self.close_button.connect("clicked", self.on_close)
+
+    @property
+    def nb_stape(self):
+        return len(self.__array_stape)
+
+    @property
+    def stape(self):
+        return self.__stape
 
     def do_close_request(self, *_args) -> bool:
         if self.accept_close:
@@ -50,3 +64,16 @@ class PyModManagerWindowModalLoad(Adw.Window):
             self.result_status.set_icon_name('process-stop-symbolic')
         self.load_stack.set_visible_child_name("result_status_page")
 
+    def add_stape(self, name, description, default_state):
+        self.__array_stape.append(StapeRow())
+        index = len(self.__array_stape) - 1
+        self.__array_stape[index].set_title(name)
+        self.__array_stape[index].set_subtitle(description)
+        self.stape_configuration.add(self.__array_stape[index])
+        return index
+
+    def choose_state(self, index, name):
+        self.__array_stape[index].choose_state(name)
+
+    def load_state(self, index, style="work"):
+        self.__array_stape[index].start_load_state(style)
