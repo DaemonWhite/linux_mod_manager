b6dbefafc9ee887163849a8639e439c8cf3b43de
DaemonWhite <matheo.daemon@gmail.com>
2024-05-12 10:50:04 +0200

    update: add_conflit_syst and post_conf

--- src/utils/current_game.py
+++ src/utils/current_game.py
@@ -1,10 +1,10 @@
 import os
 
-
-from utils.plugin_conf import PluginConfig
 from utils.xdg import xdg_conf_path
+from utils.files import generate_dict_archive, lower_case_recursif
 
 from py_mod_manager.const import USER, PLUGIN
+
 
 class CurrentGame(object):
 
@@ -24,6 +24,14 @@ class CurrentGame(object):
         return self.__current_game.name
 
     @property
+    def conflit_syst(self):
+        return self.__current_config.conflit_syst
+
+    @property
+    def post_conf(self):
+        return self.__current_config.post_conf
+
+    @property
     def plugin_conf(self):
         return self.__current_config.get_plugin_configuration("plugin_conf")
 
@@ -40,7 +48,9 @@ class CurrentGame(object):
         symbolic = False
         mode_symb = self.__settings.get_mode_symb()
         if mode_symb == USER:
-            symbolic = self.__current_config.get_plugin_configuration("symbolic")
+            symbolic = self.__current_config.get_plugin_configuration(
+                "symbolic"
+            )
         elif mode_symb == PLUGIN:
             symbolic = self.__current_game.symbolic
         else:
@@ -96,33 +106,54 @@ class CurrentGame(object):
     def set_current_game(self, current_game, current_config):
         self.__current_game = current_game
         self.__current_config = current_config
-        self.__current_config.set_path_plugin(self.__current_game.name, self.__conf_path)
+        self.__current_config.set_path_plugin(
+            self.__current_game.name,
+            self.__conf_path
+        )
         if not self.__current_game.version == self.__current_config.get_plugin_configuration("version"):
             print(f"ne correspond pas {self.__current_game.name}")
+
+    def post_traitement(self):
+        result = True
+        try:
+            lower_case_recursif(self.path_game)
+            if not self.__current_game.post_traitement():
+                return False
+            self.__current_config.enable_post_conf(True)
+        except Exception as e:
+            self.__current_config.enable_post_conf(False)
+            print("Error poste traitement : ", e)
+            result = False
+        return result
+
+    def init_conflit_syst(self):
+        result = True
+        try:
+            generate_dict_archive(False, self.path_game)
+            self.__current_config.enable_init_conflit_syst(True)
+        except Exception as e:
+            self.__current_config.enable_init_conflit_syst(False)
+            print("Error dict traitement : ", e)
+            result = False
+        return result
 
     def save_plugin(self):
         self.__current_config.save_plugin()
 
     def auto_detect_path_game(self, plugin, list_name_plugin):
         result = False
-        prefix = ""
-        install_dir = ""
         if self.__current_config.get_plugin_configuration("path") == "":
             for game_name in self.__current_game.list_name:
                 for plug_name, plugin_detect in list_name_plugin.items():
                     if plugin_detect[1].is_enable():
                         plug = plugin_detect[0]()
                         game = plug.search_game(game_name)
-                        install_dir = game.install_dir
-                        if not install_dir == "":
-                            prefix = game.prefix
+                        if not game.install_dir == "":
+                            self.path_game = game.install_dir
+                            self.path_prefix = game.prefix
                             result = True
                             break
-
-        return result, prefix, install_dir
-
-
-
+        return result
 
     def set_copy(self):
         self._default_force
