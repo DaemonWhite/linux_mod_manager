dea1a286512542b14ebc7986f5fbff7844241de9
DaemonWhite <matheo.daemon@gmail.com>
2024-05-13 19:07:17 +0200

    add: uncompress systeme

--- src/utils/files.py
+++ src/utils/files.py
@@ -1,6 +1,12 @@
 import os
 import hashlib
 
+import zipfile
+import py7zr
+import rarfile
+
+from gi.repository import Gio
+
 
 def slice_path_in_file(path):
     slice_index = path.rfind('/') + 1
@@ -54,3 +60,42 @@ def generate_dict_archive(activate_hash=False, path="/", prefix="/"):
 
     archive[prefix] = {'files': files, 'folders': folders}
     return archive
+
+
+def unzip_zip(file_path, extract_to):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+
+def unzip_7zip(file_path, extract_to):
+    with py7zr.SevenZipFile(file_path, mode='r') as z:
+        z.extractall(path=extract_to)
+
+
+def unrar(file_path, extract_to):
+    with rarfile.RarFile(file_path, 'r') as rf:
+        rf.extractall(extract_to)
+
+
+def uncompress_archive(path_file, path_dest):
+    file = Gio.file_new_for_path(path_file)
+    info = file.query_info(
+        'standard::content-type',
+        Gio.FileQueryInfoFlags.NONE,
+        None
+    )
+
+    print(path_file)
+    print(info.get_content_type())
+
+    if info.get_content_type().find("7z") >= 0:
+        unzip_7zip(path_file, path_dest)
+    elif info.get_content_type().find("rar") >= 0:
+        unrar(path_file, path_dest)
+    elif info.get_content_type().find("zip") >= 0:
+        unzip_zip(path_file, path_dest)
+    # elif info.get_content_type().find("tar") >= 0:
+    #     pass
+    else:
+        print("Format d'archive non pris en charge.")
+
